91a606e1264153f803d93e92cd2b211f647d8267
astetyne <astetyn@gmail.com>
2021-02-11 22:56:36 +0100

    Small fix with gitignore.

---
+++ core/src/com/astetyne/expirium/server/core/world/inventory/Inventory.java
@@ -0,0 +1,321 @@
+package com.astetyne.expirium.server.core.world.inventory;
+
+import com.astetyne.expirium.client.items.GridItemStack;
+import com.astetyne.expirium.client.items.Item;
+import com.astetyne.expirium.client.items.ItemStack;
+import com.astetyne.expirium.client.utils.IntVector2;
+import com.astetyne.expirium.server.core.WorldSaveable;
+import com.astetyne.expirium.server.core.entity.ExpiPlayer;
+import com.astetyne.expirium.server.core.world.file.WorldBuffer;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Notes: 0,0 is in the bottom left corner, x and y are as normal
+ */
+public class Inventory implements WorldSaveable {
+
+    protected final List<GridItemStack> items;
+    protected final GridItemStack[][] grid;
+    protected float totalWeight;
+    protected final float maxWeight;
+    protected String label;
+    protected final int rows, columns;
+    private final HashSet<ExpiPlayer> viewersSinceUpdate;
+
+    public Inventory(int rows, int columns, float maxWeight) {
+        this.rows = rows;
+        this.columns = columns;
+        this.maxWeight = maxWeight;
+        items = new ArrayList<>();
+        grid = new GridItemStack[columns][rows];
+        totalWeight = 0;
+        label = "";
+        viewersSinceUpdate = new HashSet<>();
+    }
+
+    public Inventory(int rows, int columns, float maxWeight, DataInputStream in) throws IOException {
+        this(rows, columns, maxWeight);
+        int itemsSize = in.readInt();
+        for(int i = 0; i < itemsSize; i++) {
+            GridItemStack is = new GridItemStack(in);
+            items.add(is);
+            insertToGrid(is);
+        }
+        totalWeight = in.readFloat();
+    }
+
+    public boolean canAppend(ItemStack is) {
+        return canAppend(is.getItem(), is.getAmount());
+    }
+
+    public boolean canAppend(Item item, int amount) {
+        if(totalWeight + item.getWeight() * amount > maxWeight) return false;
+        if(item.isMergeable()) {
+            for(ItemStack is : items) {
+                if(is.getItem() == item) return true;
+            }
+        }
+        for(int x = 0; x < columns; x++) {
+            for(int y = 0; y < rows; y++) {
+                if(canInsert(item, x, y)) return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean canInsert(ItemStack is, IntVector2 pos) {
+        if(totalWeight + is.getWeight() > maxWeight) return false;
+        return canInsert(is.getItem(), pos.x, pos.y);
+    }
+
+    protected boolean canInsert(Item item, int x, int y) {
+        int gw = item.getGridWidth();
+        int gh = item.getGridHeight();
+        if(gw + x > columns || gh + y > rows) return false;
+        for(int x2 = 0; x2 < gw; x2++) {
+            for(int y2 = 0; y2 < gh; y2++) {
+                if(grid[x + x2][y + y2] != null) return false;
+            }
+        }
+        return true;
+    }
+
+    public void append(ItemStack is) {
+        append(is.getItem(), is.getAmount());
+    }
+
+    public void append(Item item, int amount) {
+        if(item.isMergeable()) {
+            for(ItemStack is : items) {
+                if(is.getItem() == item) {
+                    is.increaseAmount(amount);
+                    increaseWeight(item.getWeight() * amount);
+                    refresh();
+                    return;
+                }
+            }
+        }
+        for(int y = rows - 1; y >= 0; y--) {
+            for(int x = 0; x < columns; x++) {
+                if(canInsert(item, x, y)) {
+                    GridItemStack gis = new GridItemStack(item, amount, new IntVector2(x, y));
+                    items.add(gis);
+                    insertToGrid(gis);
+                    increaseWeight(gis.getWeight());
+                    refresh();
+                    return;
+                }
+            }
+        }
+    }
+
+    public void insert(ItemStack is, IntVector2 pos) {
+        if(!canInsert(is, pos)) return;
+        GridItemStack gis = new GridItemStack(is, pos);
+        insertToGrid(gis);
+        items.add(gis);
+        increaseWeight(gis.getWeight());
+        refresh();
+    }
+
+    public void remove(ItemStack is) {
+        remove(is.getItem(), is.getAmount());
+    }
+
+    public void remove(Item item, int amount) {
+        int removedAmount = 0;
+        Iterator<GridItemStack> it = items.iterator();
+        while(it.hasNext()) {
+            GridItemStack gis = it.next();
+            if(gis.getItem() == item) {
+                int toBeRemoved = Math.min(gis.getAmount(), amount - removedAmount);
+                removedAmount += toBeRemoved;
+                gis.decreaseAmount(toBeRemoved);
+                if(gis.getAmount() == 0) {
+                    it.remove();
+                    cleanGridFrom(gis);
+                }
+                if(removedAmount == amount) {
+                    decreaseWeight(item.getWeight() * removedAmount);
+                    refresh();
+                    return;
+                }
+            }
+        }
+        // remove was not completely successful
+        decreaseWeight(item.getWeight() * removedAmount);
+        refresh();
+    }
+
+    protected void remove(IntVector2 pos) {
+        GridItemStack gis = grid[pos.x][pos.y];
+        cleanGridFrom(gis);
+        decreaseWeight(gis.getWeight());
+        items.remove(gis);
+        refresh();
+    }
+
+    public void move(IntVector2 pos1, IntVector2 pos2) {
+        GridItemStack gis1 = grid[pos1.x][pos1.y];
+        GridItemStack gis2 = grid[pos2.x][pos2.y];
+        if(gis1 == null) return;
+        if(gis1.getItem().isMergeable() && gis2 != null && gis2.getItem() == gis1.getItem() && gis1 != gis2) {
+            gis2.increaseAmount(gis1.getAmount());
+            items.remove(gis1);
+            cleanGridFrom(gis1);
+            refresh();
+            return;
+        }
+        cleanGridFrom(gis1);
+        if(!canInsert(gis1.getItem(), pos2.x, pos2.y)) {
+            insertToGrid(gis1);
+            return;
+        }
+        gis1.getGridPos().set(pos2);
+        insertToGrid(gis1);
+        refresh();
+    }
+
+    public void refresh() {
+        viewersSinceUpdate.clear();
+    }
+
+    public boolean contains(ItemStack[] itemStacks) {
+        outer:
+        for(ItemStack is : itemStacks) {
+            int remainingToFind = is.getAmount();
+            for(GridItemStack gis : items) {
+                if(gis.getItem() == is.getItem()) {
+                    remainingToFind -= gis.getAmount();
+                    if(remainingToFind <= 0) {
+                        continue outer;
+                    }
+                }
+            }
+            return false;
+        }
+        return true;
+    }
+
+    public boolean contains(Item item) {
+        for(GridItemStack is : items) {
+            if(is.getItem() == item) return true;
+        }
+        return false;
+    }
+
+    public boolean contains(Item item, int amount) {
+        for(GridItemStack is : items) {
+            if(is.getItem() == item && is.getAmount() >= amount) return true;
+        }
+        return false;
+    }
+
+    public void clear() {
+        for(GridItemStack is : items) {
+            cleanGridFrom(is);
+        }
+        totalWeight = 0;
+        items.clear();
+        refresh();
+    }
+
+    private void insertToGrid(GridItemStack gis) {
+        for(int x = 0; x < gis.getItem().getGridWidth(); x++) {
+            for(int y = 0; y < gis.getItem().getGridHeight(); y++) {
+                grid[gis.getGridPos().x+x][gis.getGridPos().y+y] = gis;
+            }
+        }
+    }
+
+    private void cleanGridFrom(GridItemStack gis) {
+        for(int x = 0; x < gis.getItem().getGridWidth(); x++) {
+            for(int y = 0; y < gis.getItem().getGridHeight(); y++) {
+                grid[gis.getGridPos().x+x][gis.getGridPos().y+y] = null;
+            }
+        }
+    }
+
+    public GridItemStack getItemOn(IntVector2 pos) {
+        return grid[pos.x][pos.y];
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public List<GridItemStack> getItems() {
+        return items;
+    }
+
+    public float getTotalWeight() {
+        return totalWeight;
+    }
+
+    public float getMaxWeight() {
+        return maxWeight;
+    }
+
+    public GridItemStack[][] getGrid() {
+        return grid;
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public int getColumns() {
+        return columns;
+    }
+
+    public void increaseWeight(float f) {
+        totalWeight += f;
+        totalWeight = Math.round(totalWeight * 100) / 100f;
+    }
+
+    public void decreaseWeight(float f) {
+        totalWeight -= f;
+        totalWeight = Math.round(totalWeight * 100) / 100f;
+    }
+
+    public boolean needsUpdate(ExpiPlayer whoAsks) {
+        return !viewersSinceUpdate.contains(whoAsks);
+    }
+
+    public void willNeedUpdate() {
+        viewersSinceUpdate.clear();
+    }
+
+    public void wasUpdated(ExpiPlayer viewer) {
+        viewersSinceUpdate.add(viewer);
+    }
+
+    @Override
+    public void writeData(WorldBuffer out) {
+        out.writeInt(items.size());
+        for(GridItemStack is : items) {
+            is.writeData(out);
+        }
+        out.writeFloat(totalWeight);
+    }
+
+    /*private void printGrid() {
+        for(int x = 0; x < columns; x++) {
+            for(int y = 0; y < rows; y++) {
+                ItemStack is = grid[x][y];
+                if(is == null) {
+                    System.out.print("[]");
+                }else {
+                    System.out.print("[" + is.getItem().name().charAt(0) + "]");
+                }
+            }
+            System.out.println();
+        }
+    }*/
+}
---
+++ core/src/com/astetyne/expirium/server/core/world/inventory/PlayerInventory.java
@@ -0,0 +1,274 @@
+package com.astetyne.expirium.server.core.world.inventory;
+
+import com.astetyne.expirium.client.items.GridItemStack;
+import com.astetyne.expirium.client.items.Item;
+import com.astetyne.expirium.client.items.ItemCat;
+import com.astetyne.expirium.client.items.ItemStack;
+import com.astetyne.expirium.client.utils.Consts;
+import com.astetyne.expirium.client.utils.IntVector2;
+import com.astetyne.expirium.server.core.WorldSaveable;
+import com.astetyne.expirium.server.core.entity.ExpiPlayer;
+import com.astetyne.expirium.server.core.world.file.WorldBuffer;
+import com.astetyne.expirium.server.net.PacketInputStream;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlayerInventory extends Inventory implements WorldSaveable {
+
+    private final ExpiPlayer owner;
+    private ItemStack itemInHand;
+    private int indexTools, indexMats, indexCons;
+    private ItemStack isTool, isMat, isCon;
+    private ChosenSlot chosenSlot;
+
+    public PlayerInventory(ExpiPlayer owner, int columns, int rows, float maxWeight) {
+        super(rows, columns, maxWeight);
+        this.owner = owner;
+        indexTools = 0;
+        indexMats = 0;
+        indexCons = 0;
+        isTool = new ItemStack(Item.EMPTY);
+        isMat = new ItemStack(Item.EMPTY);
+        isCon = new ItemStack(Item.EMPTY);
+        chosenSlot = ChosenSlot.TOOL_SLOT;
+        itemInHand = new ItemStack(Item.EMPTY);
+        label = "Note that game is still in alpha!";
+    }
+
+    public PlayerInventory(ExpiPlayer owner, int columns, int rows, float maxWeight, DataInputStream in) throws IOException {
+        super(rows, columns, maxWeight, in);
+        this.owner = owner;
+        indexTools = in.readInt();
+        indexMats = in.readInt();
+        indexCons = in.readInt();
+        isTool = new ItemStack(Item.getType(in.readInt()));
+        isMat = new ItemStack(Item.getType(in.readInt()));
+        isCon = new ItemStack(Item.getType(in.readInt()));
+        chosenSlot = ChosenSlot.getSlot(in.readInt());
+        itemInHand = new ItemStack(Item.getType(in.readInt()));
+        label = "Note that game is still in alpha!";
+    }
+
+    @Override
+    protected boolean canInsert(Item item, int x, int y) {
+        if(y == 0 && x + item.getGridWidth() >= 4) return false;
+        return super.canInsert(item, x, y);
+    }
+
+    @Override
+    public void move(IntVector2 pos1, IntVector2 pos2) {
+        GridItemStack gis = grid[pos1.x][pos1.y];
+        if(gis == null) return;
+        if(pos2.y == 0) {
+            if(pos2.x == 3) {
+                // split
+                IntVector2 appendPos = null;
+                outer:
+                for(int y = rows - 1; y >= 0; y--) {
+                    for(int x = 0; x < columns; x++) {
+                        if(canInsert(gis.getItem(), x, y)) {
+                            appendPos = new IntVector2(x, y);
+                            break outer;
+                        }
+                    }
+                }
+                if(gis.getAmount() == 1 || appendPos == null) return;
+                gis.decreaseAmount(1);
+                decreaseWeight(gis.getItem().getWeight());
+                insert(new ItemStack(gis.getItem()), appendPos);
+                willNeedUpdate();
+                return;
+            }else if(pos2.x == 4) {
+                // throw
+                remove(pos1);
+                for(int i = 0; i < gis.getAmount(); i++) {
+                    owner.getServer().getWorld().spawnDroppedItem(gis.getItem(), owner.getCenter(), Consts.ITEM_COOLDOWN_DROP);
+                }
+                willNeedUpdate();
+                return;
+            }
+        }
+        super.move(pos1, pos2);
+    }
+
+    public void onMove(Inventory second, PacketInputStream in) {
+
+        Inventory from = in.getBoolean() ? this : second; // fromMain
+        IntVector2 pos1 = in.getIntVector();
+        Inventory to = in.getBoolean() ? this : second; // toMain
+        IntVector2 pos2 = in.getIntVector();
+
+        if(from == to && pos1.y == pos2.y && pos1.x == pos2.x) return;
+
+        GridItemStack gis = from.getItemOn(pos1);
+
+        if(pos2.x == -1 || pos2.y == -1) {
+            from.remove(pos1);
+            for(int i = 0; i < gis.getAmount(); i++) {
+                owner.getServer().getWorld().spawnDroppedItem(gis.getItem(), owner.getCenter(), Consts.ITEM_COOLDOWN_DROP);
+            }
+            return;
+        }
+
+        if(from == to) {
+            from.move(pos1, pos2);
+            return;
+        }
+
+        if(gis == null) return;
+        GridItemStack gis2 = to.getItemOn(pos2);
+        if(gis.getItem().isMergeable() && gis2 != null && gis2.getItem() == gis.getItem()) {
+            gis2.increaseAmount(gis.getAmount());
+            to.increaseWeight(gis.getWeight());
+            from.remove(pos1);
+            to.refresh();
+            return;
+        }
+
+        if(!to.canInsert(gis, pos2)) return;
+        to.insert(gis, pos2);
+        from.remove(pos1);
+    }
+
+    public void onInteract(UIInteractType type) {
+
+        switch(type) {
+
+            case SLOT_TOOLS: chosenSlot = ChosenSlot.TOOL_SLOT; break;
+            case SLOT_MATERIALS: chosenSlot = ChosenSlot.MATERIAL_SLOT; break;
+            case SLOT_CONSUMABLE: chosenSlot = ChosenSlot.CONSUMABLE_SLOT; break;
+            case SWITCH_UP:
+                if(itemInHand.getItem().getCategory() == ItemCat.TOOL) {
+                    indexTools++;
+                }else if(itemInHand.getItem().getCategory() == ItemCat.MATERIAL){
+                    indexMats++;
+                }else if(itemInHand.getItem().getCategory() == ItemCat.CONSUMABLE){
+                    indexCons++;
+                }
+                break;
+            case SWITCH_DOWN:
+                if(itemInHand.getItem().getCategory() == ItemCat.TOOL) {
+                    indexTools--;
+                }else if(itemInHand.getItem().getCategory() == ItemCat.MATERIAL){
+                    indexMats--;
+                }else if(itemInHand.getItem().getCategory() == ItemCat.CONSUMABLE){
+                    indexCons--;
+                }
+                break;
+            case OPEN_INV:
+                // toto je zbytocne?
+                break;
+            case CONSUME_BUTTON:
+                if(itemInHand.getItem().getCategory() != ItemCat.CONSUMABLE) break;
+
+                switch(itemInHand.getItem()) {
+                    case APPLE:
+                        owner.increaseFoodLevel(10);
+                        remove(Item.APPLE, 1);
+                        break;
+
+                    case COOKED_APPLE:
+                        owner.increaseFoodLevel(15);
+                        remove(Item.COOKED_APPLE, 1);
+                        break;
+
+                    case RASPBERRY:
+                        owner.increaseFoodLevel(5);
+                        remove(Item.RASPBERRY, 1);
+                        break;
+
+                    case FRUIT_JAM:
+                        owner.increaseFoodLevel(25);
+                        remove(Item.FRUIT_JAM, 1);
+                        append(Item.WOODEN_BOWL, 1);
+                        break;
+                }
+                break;
+        }
+        updateHotSlots();
+    }
+
+    public void updateHotSlots() {
+
+        List<ItemStack> is1 = new ArrayList<>();
+        List<ItemStack> is2 = new ArrayList<>();
+        List<ItemStack> is3 = new ArrayList<>();
+
+        for(ItemStack is : items) {
+            if(is.getItem().getCategory() == ItemCat.TOOL) {
+                is1.add(is);
+            }else if(is.getItem().getCategory() == ItemCat.MATERIAL) {
+                is2.add(is);
+            }else if(is.getItem().getCategory() == ItemCat.CONSUMABLE) {
+                is3.add(is);
+            }
+        }
+
+        if(indexTools >= is1.size()) {
+            indexTools = 0;
+        }else if(indexTools <= -1) {
+            indexTools = is1.size() - 1;
+        }
+        if(indexMats >= is2.size()) {
+            indexMats = 0;
+        }else if(indexMats <= -1) {
+            indexMats = is2.size() - 1;
+        }
+        if(indexCons >= is3.size()) {
+            indexCons = 0;
+        }else if(indexCons <= -1) {
+            indexCons = is3.size() - 1;
+        }
+
+        if(is1.size() == 0) {
+            isTool = new ItemStack(Item.EMPTY);
+        }else {
+            isTool = is1.get(indexTools);
+        }
+        if(is2.size() == 0) {
+            isMat = new ItemStack(Item.EMPTY);
+        }else {
+            isMat = is2.get(indexMats);
+        }
+        if(is3.size() == 0) {
+            isCon = new ItemStack(Item.EMPTY);
+        }else {
+            isCon = is3.get(indexCons);
+        }
+
+        if(chosenSlot == ChosenSlot.TOOL_SLOT) {
+            itemInHand = isTool;
+        }else if(chosenSlot == ChosenSlot.MATERIAL_SLOT) {
+            itemInHand = isMat;
+        }else if(chosenSlot == ChosenSlot.CONSUMABLE_SLOT) {
+            itemInHand = isCon;
+        }
+
+        owner.getNetManager().putHotSlotsFeedPacket(chosenSlot, isTool, isMat, isCon);
+
+        for(ExpiPlayer ep : owner.getServer().getPlayers()) {
+            ep.getNetManager().putHandItemPacket(owner.getId(), itemInHand.getItem());
+        }
+
+    }
+
+    public ItemStack getItemInHand() {
+        return itemInHand;
+    }
+
+    @Override
+    public void writeData(WorldBuffer out) {
+        super.writeData(out);
+        out.writeInt(indexTools);
+        out.writeInt(indexMats);
+        out.writeInt(indexCons);
+        out.writeInt(isTool.getItem().getId());
+        out.writeInt(isMat.getItem().getId());
+        out.writeInt(isCon.getItem().getId());
+        out.writeInt(chosenSlot.getId());
+        out.writeInt(itemInHand.getItem().getId());
+    }
+}
